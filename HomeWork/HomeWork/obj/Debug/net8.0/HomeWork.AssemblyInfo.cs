ae340f6ff915c334b2d84c156fca9bd38d4a68db
Art'm <kasko.artem45@gmail.com>
2025-12-26 04:14:24 +0300

    add Swagger

--- HomeWork/HomeWork/obj/Debug/net8.0/HomeWork.AssemblyInfo.cs
+++ HomeWork/HomeWork/obj/Debug/net8.0/HomeWork.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c34b2bf5725552717c6a59a5dd5d0d67b86cc5f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
